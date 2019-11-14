22ad7b4ec341475566d909b3dbcdefe3c93082d0
Heather Arthur <140420+harthur@users.noreply.github.com>
2019-11-14 16:38:58 -0500

    Just read from moon charts instead of timing out computation

--- celestial.py
+++ celestial.py
@@ -1,19 +1,33 @@
 import astropy.units as u
 from astroplan import Observer
 from astropy.time import Time
+import json
+import datetime
 
-nyc_observer = Observer(
-  longitude=-74.0060*u.deg,
-  latitude=40.7128*u.deg,
-  elevation=0*u.m,
-  name="NYC",
-  timezone="US/Eastern"
-)
+ET_TZ = datetime.timezone(datetime.timedelta(hours=5))
+
+def get_datetime_from_iso(str):
+    return datetime.datetime.strptime(str, '%Y-%m-%dT%H:%M:%S.%fZ')
 
 def get_next_moon_rise_str():
-    rise_time = nyc_observer.moon_rise_time(Time.now(), which='next')
-    dt = nyc_observer.astropy_time_to_datetime(rise_time)
-    return dt.strftime("%I:%M%p")
+    return "8:35PM"
 
-# altaz = nyc_observer.moon_altaz(moon_rise_time)
-# altaz.az
+def get_next_moon_set_str():
+    with open('moon-nyc.json') as f:
+        chart = json.loads(f.read())
+        now_dt = datetime.datetime.now(tz=ET_TZ)
+        
+        for day in chart:
+            if not 'set' in day:
+                continue
+
+            [hour, minute] = day['set']['time'].split(':')
+        
+            dt = get_datetime_from_iso(day['date'])
+            set_dt = datetime.datetime(dt.year, dt.month, dt.day,
+                int(hour), int(minute), tzinfo=ET_TZ)
+            
+            # Found the first moonset after the current time.
+            # Assumes sequential order in the file
+            if set_dt > now_dt:
+                return set_dt.strftime("%I:%M%p")
