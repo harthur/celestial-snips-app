e65bb45dff9759e870f2bcb1974691315f5a2b6f
Heather Arthur <140420+harthur@users.noreply.github.com>
2019-12-04 15:50:28 -0500

    Display direction in one pixel

--- display.py
+++ display.py
@@ -7,28 +7,7 @@ class SenseDisplay():
   arrows and moon phases
   """
   size = 8
-  
-  @staticmethod
-  def loc(index):
-    row = math.floor(index / SenseDisplay.size)
-    col = index % SenseDisplay.size
-    return (row, col)
-    
-  # map of direction (in degrees) to pixel indices on the Sense HAT
-  # in order to display an "arrow" pointing in that direction.
-  direction_to_pixels = {
-    0: {(0,3), (0,4), (1,3), (1,4), (2,3), (2,4)},
-    45: {(0,7), (1,6), (2,5)},
-    90: {(3,5), (3,6), (3,7), (4,5), (4,6), (4,7)},
-    135: {(5,5), (6,6), (7,7)},
-    180: {(5,3), (5,4), (6,3), (6,4), (7,3), (7,4)},
-    225: {(5,2), (6,1), (7,0)},
-    270: {(3,0), (3,1), (3,2), (4,0), (4,1), (4,2)},
-    315: {(0,0), (1,1), (2,2)}
-  }
-  
-  on_color = [255, 255, 255] # white
-  off_color = [0, 0, 0] # off
+  on_color = (0, 0, 255) # blue
   
   def __init__(self):
     self.sense = SenseHat()
@@ -46,16 +25,14 @@ class SenseDisplay():
     # Adjust direction to the current orientation of the HAT 
     north = self.sense.get_compass()
     print("NORTH: %s" % north)
-    adjusted_degrees = (int(degrees) + north) % 360
-    
-    closest_cardinal = round(adjusted_degrees / 45) * 45
+    adjusted_degrees = 180 - (int(degrees) + north) % 360
 
-    on_pixels = self.direction_to_pixels[closest_cardinal]
-    pixels = [
-      self.on_color if self.loc(i) in on_pixels else self.off_color
-      for i in range(64)
-    ]
-    self.sense.set_pixels(pixels)
+    radians = math.radians(adjusted_degrees)
+    x = math.floor(math.sin(radians) * (self.size / 2 - .001) + self.size / 2)
+    y = math.floor(math.cos(radians) * (self.size / 2 - .001) + self.size / 2)
+  
+    self.sense.clear()
+    self.sense.set_pixel(int(x), int(y), self.on_color)
 
 
 display = SenseDisplay()
