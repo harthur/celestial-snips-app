8ab963bcdb39ce556aa31318fd45d59df4bcc919
Heather Arthur <140420+harthur@users.noreply.github.com>
2019-12-20 17:02:54 -0500

    Add more tests

--- test_celestial.py
+++ test_celestial.py
@@ -17,3 +17,12 @@ class TestCelestial(unittest.TestCase):
         self.assertEqual(
             self.celestial.get_next_event_after_dt(start_dt, "moon", "rise"), expected
         )
+
+    def test_get_next_event_after_dt_moon_rise_next_day(self):
+        start_dt = datetime(2019, 12, 2, 13, 0, 0, tzinfo=Celestial.ET_TZ)
+        event_dt = datetime(2019, 12, 2, 12, 15, 0, tzinfo=Celestial.ET_TZ)
+        expected = (event_dt, False, 112)
+
+        self.assertEqual(
+            self.celestial.get_next_event_after_dt(start_dt, "moon", "rise"), expected
+        )
